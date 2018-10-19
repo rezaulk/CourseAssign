54c42af9aad8750cbda693cb9baad4428de6ab6c
rezaulk <rezaul.bd77@gmail.com>
2018-10-19 18:23:00 +0600

    entities

---
+++ CourseAssign/CourseAssign.Core.Entities/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseAssign.Core.Entities
+{
+    class Teacher
+    {
+        [Key]
+        public string Id { get; set; }
+        public int Name { get; set; }
+        public string PhoneNo { get; set; }
+        public string Password { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+
+    }
+}
